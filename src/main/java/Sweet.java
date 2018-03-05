79d6b7a34245bec27b68c8f3ef58b979f75e17f3
daniel kelly <danieljkelly91@gmail.com>
2018-03-05 12:12:31 +0000

    adds customer test

--- src/main/java/Sweet.java
+++ src/main/java/Sweet.java
@@ -1,4 +1,6 @@
 public class Sweet extends Product {
+    private int weight;
+
     public Sweet(String name, int weight) {
         super(name);
         this.weight = weight;
---
+++ src/test/java/TestCustomer.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCustomer {
+
+    private Customer customer;
+
+    @Before
+    public void setUp() throws Exception {
+        customer = new Customer("John", 10.00);
+    }
+
+    @Test
+    public void testName() {
+        assertEquals("John", customer.getName());
+    }
+}
