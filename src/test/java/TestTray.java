7de0ce5021edbdd49568d7a6287afb5973bb0daf
daniel kelly <danieljkelly91@gmail.com>
2018-03-05 13:31:37 +0000

    added testTray

---
+++ src/test/java/TestTray.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTray {
+
+    private Tray tray;
+
+    @Before
+    public void setUp() throws Exception {
+        //arraylist should be empty to start so don't need it initialize it
+        //pass price in initally and capacity
+        tray = new Tray(0.50, 10);
+    }
+
+    @Test
+    public void testTrayPrice() {
+        assertEquals(0.50, tray.getPrice(), 0.01);
+    }
+
+    //check trays have a price
+
+    //check trays report the number of items they contain
+
+    // check we can add item to tray
+
+    //check we can remove item from tray
+
+    //check that we can't add items beyond a tray's capacity
+
+
+}
