ebab7e05f9b048e1ed334c85a169f2798a57c23c
daniel kelly <danieljkelly91@gmail.com>
2018-03-05 13:34:45 +0000

    added Tray class with constructor and getPrice method

---
+++ src/main/java/Tray.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Tray {
+
+    private ArrayList<Product> stock;
+    private double price;
+    private int capacity;
+
+    public Tray(double price, int capacity) {
+        this.price = price;
+        this.capacity = capacity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
