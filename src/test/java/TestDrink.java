1a8c863851aa6a58fed10b0c8d35ba9565011f6d
daniel kelly <danieljkelly91@gmail.com>
2018-03-05 11:53:50 +0000

    Adds setup in Drink test

--- src/test/java/TestDrink.java
+++ src/test/java/TestDrink.java
@@ -1,2 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestDrink {
+
+    private Drink drink;
+
+    @Before
+    public void setUp() throws Exception {
+        drink = new Drink("Pepsi", 330);
+    }
+
+
+
+
 }
